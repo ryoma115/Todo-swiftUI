1b270f2727b76cf7bc314d708e087fce89aaaf5f
ryoma115 <nakayamaryoma@gmail.com>
2022-04-25 13:07:09 +0900

    セクション5

---
+++ Todo/CheckBox.swift
@@ -0,0 +1,42 @@
+//
+//  CheckBox.swift
+//  Todo
+//
+//  Created by N. Ryoma on 2022/04/25.
+//
+
+import SwiftUI
+
+struct CheckBox<Label>: View where Label: View{
+    @Binding var checked: Bool
+    
+    private var label: ()-> Label
+    
+    public init(checked: Binding<Bool>, @ViewBuilder label: @escaping ()-> Label) {
+        self._checked = checked
+        self.label = label
+    }
+    
+    var body: some View {
+        HStack {
+            Image(systemName: checked ? "checkmark.circle" : "circle")
+                .onTapGesture {
+                    self.checked.toggle()
+                
+            }
+            label()
+        }
+    }
+}
+
+struct CheckBox_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            CheckBox(checked: .constant(false)) {
+                Image(systemName: "hand.thumbsup")
+                Text("牛乳を買う")
+                Text("牛乳を買う")
+            }
+        }
+    }
+}
